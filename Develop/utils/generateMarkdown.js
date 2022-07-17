06a2b477511eded466e95bcbd1629025862ef5f1
jfranklin12 <j.franks725@gmail.com>
2022-07-17 11:49:32 -0400

    functions for licence badge and license link created

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == 'MIT'){
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license == 'APACHE 2.0'){
+    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  } else if (license == 'GPL 3.0'){
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  }else if (license == 'BSD 3.0'){
+    return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+  }else if (license == 'Boost'){
+    return `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+  }else{
+    return ''
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == 'MIT'){
+    return `https://opensource.org/licenses/MIT`
+  } else if (license == 'APACHE 2.0'){
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (license == 'GPL 3.0'){
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  }else if (license == 'BSD 3.0'){
+    return `https://opensource.org/licenses/BSD-3-Clause`
+  }else if (license == 'Boost'){
+    return `https://www.boost.org/LICENSE_1_0.txt`
+  }else{
+    return ''
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
